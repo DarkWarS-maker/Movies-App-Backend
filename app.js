f846cac0ef64df7fcbb07a0eedb71b7f7e9e606f
DarkWarS <ds1830056@gmail.com>
2022-06-12 23:23:31 +0530

    api

--- app.js
+++ app.js
@@ -14,11 +14,11 @@ await mongooseConnect();
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://62a5f7b63eb97f713b416675--superlative-vacherin-491061.netlify.app/"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "https://62a5f7b63eb97f713b416675--superlative-vacherin-491061.netlify.app"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
-// app.use(cors({ credentials: false , origin:""}));
+// app.use(cors());
 //app.set("trust proxy", 1);
 
 app.get("/", (req, res) => res.send("server running"));
